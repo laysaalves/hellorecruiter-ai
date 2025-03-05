8e8729c939864e0a9a1c24b50281c94536ac16f4
Layseiras <layseirasdev@outlook.com>
2025-03-05 09:51:56 -0300

    first commit

---
+++ src/main/java/dev/layseiras/hellorecruiter/HellorecruiterApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.hellorecruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorecruiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorecruiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/hellorecruiter/HellorecruiterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.hellorecruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorecruiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
